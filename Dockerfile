501262fcaec1d6c2e01c9efcd8a5b479c180ff2c
2000kumaramresh <kumaramresh2000@gmail.com>
2023-11-18 11:31:38 +0530

    web deployment assignment

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.9.17
 COPY . /digits/
 RUN pip3 install --no-cache-dir -r /digits/requirements.txt
 WORKDIR /digits
-CMD ["python3","exp.py"]
+RUN python3 exp.py
 
 ENV FLASK_APP =app.py
 CMD ["flask", "run"]
--- api/app.py
+++ api/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, request
-app = Flask(_name_)
+import joblib
+import numpy as np
+
+app = Flask(__name__)
 
 @app.route("/hello/<user>")
 def hello_world(user):
@@ -12,6 +15,25 @@ def sum(x,y):
 @app.route("/model", methods=['POST'])
 def pred_model():
     js = request.get_json()
-    x = js['x']
-    y = js['y']
-    return str(int(x) + int(y))
+        
+    image = np.array(js['image']).reshape(1, -1)
+    model = joblib.load('Models/svm_gamma:0.0005_C:1.joblib')
+    predict = model.predict([image])
+    return str(predict)
+
+@app.route("/compare", methods=['POST'])
+def compare_images():
+    js = request.get_json()
+    image1 = np.array(js['image1'])
+    image2 = np.array(js['image2'])
+
+    image1.reshape(1, -1)
+    image2.reshape(1, -1)
+    model = joblib.load('Models/svm_gamma:0.0005_C:1.joblib')
+    predict = model.predict([image1,image2])
+    # predict2 = model.predict(image2)
+
+    if( (predict[0] == predict[1])):
+        return f"True"
+    else:
+        return f"False"
